1a9289dd29bfa9d3defa931a713457f72d1dadea
Gluggsi <42485822+Gluggsi@users.noreply.github.com>
2018-08-18 13:31:42 +0800

    Update collection.php

--- collection.php
+++ collection.php
@@ -1,2 +1,5 @@
 // Sanitise a user entered string before Database storage
 $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+// Check if this is a post
+if ( $_SERVER['REQUEST_METHOD'] == 'POST')
