fa7ba7aa055722238c800c336952bef98c41be8e
Gluggsi <42485822+Gluggsi@users.noreply.github.com>
2018-08-23 15:45:41 +0800

    Update collection.php

--- collection.php
+++ collection.php
@@ -3,3 +3,9 @@ $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 
 // Check if this is a post
 if ( $_SERVER['REQUEST_METHOD'] == 'POST')
+
+// Remove white-spaces
+$value = preg_replace( '/\s+/', ' ', $value );
+
+// Remove multiple white-spaces, but leave one
+$value = preg_replace('/\s{2,}/',' ', $value);
